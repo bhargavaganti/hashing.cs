5311ea5a875fbf4ea1770f9f19992077a81148ed
server <server@example.com>
2018-02-03 21:50:16 -0700

    test in progress

--- src/hashing.classes/hasher.cs
+++ src/hashing.classes/hasher.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
 
 namespace hashing.classes
 {
@@ -23,5 +26,66 @@ namespace hashing.classes
 
             return hash;
         }
+
+        /**
+         * Generating random string multiple times should not result same.
+         */
+        public string random()
+        {
+            int length = 20;
+
+            Random random = this.randomizer();
+            string chars = this.randomCharacters();
+            string r = new string(Enumerable.Repeat(chars, length).Select(s => s[random.Next(s.Length)]).ToArray());
+
+            return r;
+        }
+
+        private string randomCharacters()
+        {
+            Random random = this.randomizer();
+
+            string chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890";
+            //chars = Enumerable.Range(0, chars.Length).OrderBy(ri => random.Next()).ToArray().ToString();
+            int offset = 1 + 5;
+            int c = random.Next(0, chars.Length); // - offset);
+            string first = chars.Substring(0, c);
+            string second = chars.Substring(c, chars.Length - c);
+            chars = string.Format("{0}{1}", second, first);
+            return chars;
+        }
+
+        private Random randomizer()
+        {
+            //Random random = new Random(Guid.NewGuid().GetHashCode());
+            Random random = new Random(new System.DateTime().Millisecond);
+            return random;
+        }
+
+        /**
+         * Generating random string multiple times should not result same.
+         *
+         * @see https://stackoverflow.com/questions/1344221/how-can-i-generate-random-alphanumeric-strings-in-c
+         */
+        public string r()
+        {
+            int maxSize = 20;
+
+            char[] chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890".ToCharArray();
+            byte[] data = new byte[1];
+            using (RNGCryptoServiceProvider crypto = new RNGCryptoServiceProvider(Guid.NewGuid().ToString()))
+            {
+                crypto.GetNonZeroBytes(data);
+                data = new byte[maxSize];
+                crypto.GetNonZeroBytes(data);
+            }
+            StringBuilder result = new StringBuilder(maxSize);
+            foreach (byte b in data)
+            {
+                result.Append(chars[b % (chars.Length)]);
+            }
+            return result.ToString();
+        }
     }
 }
+ 
---
+++ src/hashing.tests/hasherTests.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using hashing.classes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hashing.classes.Tests
+{
+    [TestClass()]
+    public class hasherTests
+    {
+        [TestMethod()]
+        public void randomTest()
+        {
+            hasher h = new hasher();
+
+            string r1 = h.random();
+            string r2 = h.random();
+
+            Assert.AreNotEqual(r1, r2);
+        }
+
+        [TestMethod()]
+        public void rTest()
+        {
+            hasher h = new hasher();
+
+            string r1 = h.r();
+            string r2 = h.r();
+
+            Assert.AreNotEqual(r1, r2);
+        }
+    }
+}
